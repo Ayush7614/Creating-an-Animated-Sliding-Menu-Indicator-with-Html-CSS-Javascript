f04f88a32814cd285b7cf44c4bc0e6c3250eff9d
Ayush kumar <67006255+Ayush7614@users.noreply.github.com>
2020-09-17 14:03:20 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+    <script>
+        const marker = document.querySelector('#marker');
+        const item = document.querySelectorAll('ul li a');
+
+        function indicator(e){
+            marker.style.top = e.offsetTop+'px';
+            marker.style.width = e.offsetWidth+'px';
+        }
+        item.forEach(link => {
+            link.addEventListener('mousemove', (e) => {
+                indicator(e.target);
+
+            })
+        })
+
+    </script>
